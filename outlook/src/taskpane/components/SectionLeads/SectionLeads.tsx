af92d53c5e6cb1d7e24a960949cd2e44358a2513
amcgeough <andrewmcgeough@gmail.com>
2023-01-06 13:10:30 +0000

    email search working on opps

--- outlook/src/taskpane/components/SectionLeads/SectionLeads.tsx
+++ outlook/src/taskpane/components/SectionLeads/SectionLeads.tsx
@@ -18,172 +18,37 @@ type SectionLeadsState = {
     email_leads: Lead[];
     replyto: string;
 };
-let originalEmailID;
-let email_leads;
 
 class SectionLeads extends React.Component<LeadSectionProps, SectionLeadsState> {
     constructor(props, context) {
         super(props, context);
-        email_leads = this.props.partner.leads
-        this.state = {email_leads, replyto: null };
-   
+        this.state = {email_leads: this.props.partner.leads, replyto: null};    
     }
-      
-    
-    async componentDidMount() {
-        Office.context.mailbox.item.getAllInternetHeadersAsync((asyncResult) => {
-          if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-            const headers = asyncResult.value;
-            if (headers) {
-              const replyto = headers.match(/In-Reply-To: (.*)/);
-              if (replyto) {
-                email_leads = this.props.partner.leads.filter(lead => lead.id === 28);
-                const email_leads2 = this.props.partner.leads.filter(lead => lead.id === 28);
-                this.setState({email_leads: email_leads2, replyto: replyto[1] });
-              }
+
+    async componentDidMount() {this.EmailFilter();}
+         
+    async EmailFilter() {
+    Office.context.mailbox.item.getAllInternetHeadersAsync((asyncResult) => {
+        if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
+          const headers = asyncResult.value;
+          if (headers) {
+            const replyto = headers.match(/In-Reply-To: (.*)/);
+            const email_leads = this.props.partner.leads.filter(lead => lead.id === 283333);
+            if (email_leads.length > 0) {
+              this.setState({ email_leads: email_leads, replyto: replyto[1] });
+            }
+            else
+            {
+              this.setState({ email_leads: this.props.partner.leads, replyto: replyto[1] });
             }
           }
-        });
-      }
-    
-
-  
-    async getEmailHeaders2() {
-        return new Promise((resolve, reject) => {
-          Office.context.mailbox.item.getAllInternetHeadersAsync(function (asyncResult) {
-            if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-              let headers = asyncResult.value;
-              let replyto = headers.match(/In-Reply-To: (.*)/);
-              console.log('replyto: ' + replyto);
-              if (replyto) {
-                // let originalEmailID = replyto[1];
-                const email_leads2 = email_leads.filter(lead => lead.id === 28);
-                console.log('email_leads2: ' + email_leads2);
-                resolve(email_leads2);
-              }
-            
-              else {
-                reject();
-              }
-            } else {
-              reject();
-            }
-          });
-        });
-      }
-    
-    async fetchReplyTo() {
-        console.log('getEmailHeaders function called'); // Debugging statement
-        // return new Promise((resolve, reject) => {
-          Office.context.mailbox.item.getAllInternetHeadersAsync(function (asyncResult) {
-            // if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-            //   console.log('getAllInternetHeadersAsync succeeded'); // Debugging statement
-              let headers = asyncResult.value;
-              let replyto = headers.match(/In-Reply-To: (.*)/);
-              if (replyto) {
-                originalEmailID = replyto[1]; // Assign the value to the global variable
-                console.log('originalEmailID: ' + originalEmailID);
-                const email_leads2 = this.props.partner.leads.filter(lead => lead.id === 28);
-                //   await this.setState({ originalEmailID: originalEmailID });
-                //   this.setState({ leads: email_leads });
-                        // resolve(originalEmailID); // Resolve the promise with the originalEmailID value
-                return email_leads2
-              } else {
-                console.log('Reply To attribute not found');
-                return null;
-                // reject(); // Reject the promise
-              }
-            // } else {
-            //   console.error(asyncResult.error.message);
-            //   reject(); // Reject the promise
-            // }
-          });
-        };
-        
-
-        async test() {
-            return new Promise((resolve, reject) => {
-                Office.context.mailbox.item.getAllInternetHeadersAsync(function (asyncResult) {
-                            let headers = asyncResult.value;
-                            let replyto = headers.match(/In-Reply-To: (.*)/);
-                            if (replyto) {
-                                originalEmailID = replyto[1]; // Assign the value to the global variable
-                                console.log('originalEmailID: ' + originalEmailID);
-                                email_leads = email_leads.filter(lead => lead.id === 28);
-                                console.log(email_leads);
-                                this.setState({ leads: email_leads });
-                                // this.state = { leads: this.props.partner.leads };
-                                // console.log(this.state); // state should be defined here
-                                resolve(email_leads);
-                                return email_leads;
-                            } else {
-                                console.log('Reply To attribute not found');
-                                reject();
-                                return null;
-                            }
-                        })})
+          else {
+            this.setState({ email_leads: this.props.partner.leads, replyto: 'No Header' });
+          }
         }
-        
-        // test().then(result => {
-        //     console.log(result);
-        //     email_leads = result;
-        //     console.log(email_leads);
-        //     this.setState({ leads: email_leads });
-        //     console.log(this.state);
-        //   });
-        
-        // console.log(this.state.leads);
+      });
+    }
 
-
-
-        getEmailHeaders() {
-            console.log('getEmailHeaders function called'); // Debugging statement
-            // return new Promise((resolve, reject) => {
-              Office.context.mailbox.item.getAllInternetHeadersAsync(function (asyncResult) {
-                // if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
-                //   console.log('getAllInternetHeadersAsync succeeded'); // Debugging statement
-                  let headers = asyncResult.value;
-                  let replyto = headers.match(/In-Reply-To: (.*)/);
-                  if (replyto) {
-                    originalEmailID = replyto[1]; // Assign the value to the global variable
-                    console.log('originalEmailID: ' + originalEmailID);
-                    email_leads = this.state.leads.filter(lead => lead.id === 28);
-                    //   await this.setState({ originalEmailID: originalEmailID });
-                    //   this.setState({ leads: email_leads });
-                            // resolve(originalEmailID); // Resolve the promise with the originalEmailID value
-                    return email_leads
-                  } else {
-                    console.log('Reply To attribute not found');
-                    return null;
-                    // reject(); // Reject the promise
-                  }
-                // } else {
-                //   console.error(asyncResult.error.message);
-                //   reject(); // Reject the promise
-                // }
-              });
-            };
-
-        // async main() {
-
-        //     try {
-        //       console.log('main function called'); // Debugging statement
-        //         // this.getEmailHeaders();
-        //         console.log('originalEmailID: ' + originalEmailID);
-        //       email_leads = this.state.leads.filter(lead => lead.id === 28);
-        //     //   await this.setState({ originalEmailID: originalEmailID });
-        //       this.setState({ leads: email_leads });
-        //         console.log(this.state.leads);
-        //       console.log('main function finished'); // Debugging statement
-        //     } catch (error) {
-        //       console.error(error); // Log the error to the console
-        //     }
-
-        // }
-
-        
-
-        
 
     private getLeadDescription = (lead: Lead): string => {
         const expectedRevenueString = _t(
@@ -201,23 +66,20 @@ class SectionLeads extends React.Component<LeadSectionProps, SectionLeadsState> 
         return expectedRevenueString;
     };
 
+      
+
     render() 
     {    
-        // this.test().then(result => {
-        //         console.log(result);
-        //         // email_leads = result;
-        //         // console.log(email_leads);
-        //         email_leads = this.state.leads.filter(lead => lead.id === 28);
-        //         // this.setState({ leads: email_leads });
-        //         // console.log(this.state);
-        //       });
         console.log(this.state.replyto);
         console.log(this.state.email_leads);
-        console.log(email_leads);
+  
+        if (this.state.replyto === null) {return <p>hello yaasss</p>} 
+        else {
+
         return (
             
             <Section
-                records={email_leads}
+                records={this.state.email_leads}
                 partner={this.props.partner}
                 canCreatePartner={this.props.canCreatePartner}
                 model="crm.lead"
@@ -232,8 +94,10 @@ class SectionLeads extends React.Component<LeadSectionProps, SectionLeadsState> 
                 msgLogEmail="Log Email Into Lead"
                 getRecordDescription={this.getLeadDescription}
             />
-        );
+        );}
     }
+
+    
       
 }
 
